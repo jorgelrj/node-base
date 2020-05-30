cad80e476c2006ccb4489594f47ef3bfd928c3ba
Jorge Luís <jr.luisrodrigues@gmail.com>
2020-05-30 09:51:37 -0300

    feat(*): Add new folders and plugins

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,34 @@
-import express from 'express';
+// import 'reflect-metadata';
+
+// import './database';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
+import AppError from './errors/AppError';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json);
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
-  // eslint-disable-next-line no-console
   console.log('🚀 Running');
 });
